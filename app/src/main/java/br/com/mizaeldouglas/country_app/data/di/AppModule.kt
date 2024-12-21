35505479bd0c329135acc0899a19cdf1ebf48c83
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2024-12-21 13:17:13 -0300

    feat: created inject dependency

--- app/src/main/java/br/com/mizaeldouglas/country_app/data/di/AppModule.kt
+++ app/src/main/java/br/com/mizaeldouglas/country_app/data/di/AppModule.kt
@@ -1,4 +1,61 @@
 package br.com.mizaeldouglas.country_app.data.di
 
-class AppModule {
+import br.com.mizaeldouglas.country_app.data.api.CountryApi
+import br.com.mizaeldouglas.country_app.data.api.CountryService
+import br.com.mizaeldouglas.country_app.data.repository.CountryRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    private const val BASE_URL = "https://restcountries.com/v3.1/"
+
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCountryApi(retrofit: Retrofit): CountryApi {
+        return retrofit.create(CountryApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCountryService(countryApi: CountryApi): CountryService {
+        return CountryService(countryApi)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCountryRepository(countryService: CountryService): CountryRepository {
+        return CountryRepository(countryService)
+    }
 }
