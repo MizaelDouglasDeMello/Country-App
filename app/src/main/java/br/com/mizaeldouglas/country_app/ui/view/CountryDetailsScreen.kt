a4e27899252539d21e4c2c6328d67ff37f83d508
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2024-12-22 11:25:03 -0300

    feat: fixing bugs

--- app/src/main/java/br/com/mizaeldouglas/country_app/ui/view/CountryDetailsScreen.kt
+++ app/src/main/java/br/com/mizaeldouglas/country_app/ui/view/CountryDetailsScreen.kt
@@ -1,12 +1,26 @@
-import androidx.compose.foundation.layout.*
+import android.app.Activity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.*
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -16,19 +30,26 @@ import coil.compose.AsyncImage
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CountryDetailsScreen(country: Country, navController: NavController) {
+    val activity = LocalContext.current as? Activity
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text(text = country.name.common) },
                 navigationIcon = {
-                    IconButton(onClick = { navController.popBackStack() }) {
+                    IconButton(onClick = {
+                        val isPopped = navController.popBackStack()
+                        if (!isPopped) {
+                            activity?.finish()
+                        }
+                    }) {
                         Icon(
-                            imageVector = Icons.Default.ArrowBack, // Ícone padrão de "voltar"
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "Back"
                         )
                     }
                 }
             )
+
         }
     ) { paddingValues ->
         Column(
